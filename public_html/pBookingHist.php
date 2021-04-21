c2dd98d2cb9dae7a1636116929b69ddc00d8d33f
Kananelo <69701344+kananelomokoko1@users.noreply.github.com>
2021-04-21 16:38:50 +0200

    Updated current public_html file
    I removed dHistoryItem.php and pHistoryItem.php.
    I completed dBookingHist.php(doctor booking history) and pBookingHist.php(patient booking history)

--- public_html/pBookingHist.php
+++ public_html/pBookingHist.php
@@ -3,4 +3,21 @@ require "conn.php";
 #retrieve all the patient's booking history
 #for an individual file doctor'sname, doctor type, date of visit
 
+$patient_userid = $_POST["patient_userid"];
+$p_id = (int)$patient_userid;
+
+if($conn){
+    $patient_query = "SELECT * FROM BOOKINGS WHERE PATIENT_NO= '$p_id' AND STATUS='FULFILLED'";
+
+    $sql_patientQ = mysqli_query($conn,$patient_query);
+    $output = array();
+    if($result=$sql_patientQ){
+        while($row = $result->fetch_assoc()){
+            $output [] = $row;
+        }
+    }
+}
+mysqli_close($conn);
+echo json_encode($output);
+
 ?>
