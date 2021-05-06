f677cc62f91faf3563afe82fea735b5e48f8c810
Phindulo60 <1832463@students.wits.ac.za>
2021-05-07 00:09:50 +0200

    clicks

--- app/src/main/java/com/killmongerscode/aid/Doctor_Homepage.java
+++ app/src/main/java/com/killmongerscode/aid/Doctor_Homepage.java
@@ -1,14 +1,35 @@
 package com.killmongerscode.aid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
 
 public class Doctor_Homepage extends AppCompatActivity {
+
+    GridLayout doctor_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor__homepage);
+
+        doctor_home = (GridLayout)findViewById(R.id.doctor_home);
+
+        setSingleEvent(doctor_home);
+    }
+
+    private void setSingleEvent(GridLayout doctor_home) {
+        for (int i = 0; i < doctor_home.getChildCount();i++)
+        {
+
+            CardView cardView = (CardView)doctor_home.getChildAt(i);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {}
+            });
+        }
     }
 }
--- app/src/main/java/com/killmongerscode/aid/Patient_Homepage.java
+++ app/src/main/java/com/killmongerscode/aid/Patient_Homepage.java
@@ -1,14 +1,42 @@
 package com.killmongerscode.aid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.EventListener;
 
 public class Patient_Homepage extends AppCompatActivity {
+
+    GridLayout Patient_homepage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient__homepage);
+
+        Patient_homepage = (GridLayout)findViewById(R.id.Patient_homepage);
+
+        setSingleEvent(Patient_homepage);
+    }
+
+    private void setSingleEvent(GridLayout Patient_homepage) {
+        for (int i = 0; i < Patient_homepage.getChildCount();i++)
+        {
+
+            CardView cardView = (CardView)Patient_homepage.getChildAt(i);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {}
+            });
+        }
     }
 }
