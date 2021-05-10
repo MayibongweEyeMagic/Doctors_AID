ffcd97e94b3748bb73590bdee52db03472948355
Lethokuhle123 <1975506@students.wits.ac.za>
2021-05-10 14:11:54 +0200

    Added doctor's profile layout

--- app/src/main/java/com/killmongerscode/aid/PatientProfile.java
+++ app/src/main/java/com/killmongerscode/aid/PatientProfile.java
@@ -3,12 +3,20 @@ package com.killmongerscode.aid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class PatientProfile extends AppCompatActivity {
 
+    private EditText Name, Surname, Cellphone, Email, Location, Lastvisit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_profile);
+        Name = findViewById(R.id.name);
+        Surname = findViewById(R.id.surname);
+        Cellphone = findViewById(R.id.cellphone);
+        Email = findViewById(R.id.email);
+        Location = findViewById(R.id.location);
+        Lastvisit = findViewById(R.id.lastvisit);
     }
 }
