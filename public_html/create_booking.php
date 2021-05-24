224746412815fcaf4a6dd70da1f154e4e79893ee
Kananelo <69701344+kananelomokoko1@users.noreply.github.com>
2021-05-24 14:38:56 +0200

    Update create_booking.php

--- public_html/create_booking.php
+++ public_html/create_booking.php
@@ -4,14 +4,14 @@
 $patient = $_POST["email"]; //use email to uniquely identify individual
 // DATE MUST BE YYYY-MM-DD HH:MM:SS
 $booking_date = $_POST["date"]; //this is the time and date that the user has made the appointment to be
-$doctor_lname = $_POST["lastname"];
-$doctor_fname = $_POST["firstname"];
+/*$doctor_lname = $_POST["lastname"];
+$doctor_fname = $_POST["firstname"];*/
 $dr_email = $_POST["email"];
 $reason = $_POST["reason"]; //why visit
 $doctor_spec = $_POST["specialisation"];
 //should I add doctor experience?
 
-if(!empty($patient) && !empty($booking_date) && !empty($doctor_fname) && !empty($doctor_lname) && !empty($doctor_spec) && !empty($dr_email)){
+if(!empty($patient) && !empty($booking_date) && /*!empty($doctor_fname) && !empty($doctor_lname) &&*/ !empty($doctor_spec) && !empty($dr_email)){
     /*$drQuery = "SELECT DOCTOR_EMAIL FROM DOCTOR WHERE DOCTOR_FNAME LIKE '$doctor_fname' AND DOCTOR_LNAME LIKE '$doctor_lname' AND DOCTOR_SPEC LIKE '$doctor_spec'";
     if (mysql_num_rows($drQuery)==1) {*/
 
