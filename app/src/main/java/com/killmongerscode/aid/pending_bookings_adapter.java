3b6a75c89bf3ca65e378d42f3cdd66093bc08185
Sibabalo <1963321@students.wits.ac.za>
2021-05-28 00:00:14 +0200

    Successful Booking

--- app/src/main/java/com/killmongerscode/aid/pending_bookings_adapter.java
+++ app/src/main/java/com/killmongerscode/aid/pending_bookings_adapter.java
@@ -16,21 +16,42 @@ public class pending_bookings_adapter extends RecyclerView.Adapter<pending_booki
     private ArrayList<Patient> patientList;
     private Context context;
 
+    private OnItemCLickListener Listener;
+
+    public interface OnItemCLickListener{
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemCLickListener listener){
+        Listener =listener;
+    }
+
     public pending_bookings_adapter(ArrayList<Patient>patientList, Context context){
         this.patientList = patientList;
         this.context = context;
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
         private TextView Nametext, surname, email;
 
 
-        public MyViewHolder(final View view){
+        public MyViewHolder(final View view, OnItemCLickListener lickListener){
             super(view);
             Nametext =view.findViewById(R.id.Patient_Name);
             surname =view.findViewById(R.id.Surname);
             email =view.findViewById(R.id.patient_email);
 
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                        if (lickListener != null){
+                            int position =getAdapterPosition();
+                            if (position != RecyclerView.NO_POSITION){
+                                lickListener.onItemClick(position );
+                            }
+
+                        }                }
+            });
         }
 
     }
@@ -39,7 +60,7 @@ public class pending_bookings_adapter extends RecyclerView.Adapter<pending_booki
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View patientview = LayoutInflater.from(parent.getContext()).inflate(R.layout.pending_bookings, parent, false);
-        return new MyViewHolder(patientview);
+        return new MyViewHolder(patientview, Listener);
     }
 
     @Override
