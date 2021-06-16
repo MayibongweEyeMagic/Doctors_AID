6a20308bff86cf70df5a81577a96ccda0925d7c5
khananie <42744564+khananie@users.noreply.github.com>
2021-06-16 19:37:57 +0200

    Add files via upload

---
+++ app/src/main/java/com/killmongerscode/aid/Doctor.java
@@ -0,0 +1,78 @@
+package com.killmongerscode.aid;
+
+public class Doctor {
+    private String name;
+    private String surname;
+    private String tel;
+    private String email;
+    private String specialite;
+    private String university;
+    private String qualification;
+
+
+    public Doctor(){
+        this.name = name;
+        this.surname = surname;
+        this.tel = tel;
+        this.email = email;
+        this.specialite = specialite;
+        this.university = university;
+        this.qualification = qualification;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSpecialite() {
+        return specialite;
+    }
+
+    public void setSpecialite(String specialite) {
+        this.specialite = specialite;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+}
